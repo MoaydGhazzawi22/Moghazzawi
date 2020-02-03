1e6e2af43e34fe268841e0bb3574919aae2faa3c
Moayd Ghazzawi <mghazzawi@student.cccd.edu>
2020-02-03 17:16:39 +0000

    auto commit 02/03/20 17:16:39

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -13,8 +13,9 @@ extern string ASSIGNMENT;
 
 
 /**
- * Give me a time (such as 3:57) and a duration
- * (such as 1:05), and I'll tell you the sum and difference.
+ * Give me a time and a duration
+ *  and I will tell you the sum and difference.
+ *
  * One line describing what this program does.
  * @return 0 for success.
  */
@@ -22,9 +23,10 @@ extern string ASSIGNMENT;
 
 int run()
 {
-        // 1. Title and introduction
-        cout << STUDENT << " " << ASSIGNMENT << endl;
-        cout << "___________________________________________" << endl;
+        int time, duration, after, before, afterHours, afterMinutes;
+        int beforeHours, beforeMinutes;
+        int timeHours, timeMinutes, durationHours, durationMinutes;
+        char discard;
         cout << "Give me a time (such as 3:57) and a duration" << endl;
         cout << "(such as 1:05), and I'll tell you the sum" << endl;
         cout << "(that is, the time that follows the given time" << endl;
@@ -34,53 +36,38 @@ int run()
         cout << endl;
 
 
-    // 2. Input section: prompt and input on same line
-
-        int TIME_HOURS, TIME_MINUTES, DURATION_HOURS, DURATION_MINUTES;
-        char discard;
 
         cout << "Time:" ;
-        cin >> TIME_HOURS >> discard >> TIME_MINUTES;
+        cin >> timeHours >> discard >> timeMinutes;
 
 
 
 
         cout << "Duration:" ;
-        cin >> DURATION_HOURS >> discard >> DURATION_MINUTES;
+        cin >> durationHours >> discard >> durationMinutes;
         cout << endl;
 
 
-    // 3. Processing section - compute the results
-
-    int time, duration, after, before, afterHours, afterMinutes;
-    int beforeHours, beforeMinutes;
 
 
-    time = TIME_HOURS * 60 + TIME_MINUTES;
-    duration = DURATION_HOURS * 60 + DURATION_MINUTES;
+          time = timeHours * 60 + timeMinutes;
+        duration = durationHours * 60 + durationMinutes;
 
 
-    after = time + duration;
-    before = time - duration;
+         after = time + duration;
+          before = time - duration;
 
     afterHours = (after / 60);
-    afterMinutes = (after)%60;
+          afterMinutes = (after)%60;
 
-    beforeHours = (before/60);
-    beforeMinutes = before%60;
-cout<< "beforeMinutes -->" << beforeMinutes;
-
-
-
-    // - blank line
+        beforeHours = (before/60);
+        beforeMinutes = before%60;
+          cout<< "beforeMinutes -->" << beforeMinutes;
 
        cout << endl;
 
-
-    // 4. Output section: test data inside brackets
-
-        cout << DURATION_HOURS << ":" << DURATION_MINUTES << " hours after, and before, "
-            << TIME_HOURS << ":" << TIME_MINUTES
+        cout << durationHours << ":" << durationMinutes << " hours after, and before, "
+            << timeHours << ":" << timeMinutes
 
             << setfill('0') << " is ["<< (afterHours + 11)%12+1<< ":" << setw(2) << (afterMinutes + 1440)%60 << ", "
             <<  (beforeHours + 11)%12 + 1<< ":" << setw(2) << (beforeMinutes + 1440)%60<< "]" << endl;
