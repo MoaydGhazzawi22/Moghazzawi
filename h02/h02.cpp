17ee98f313b4b24e9120a9a711394c0c3ed8b1ce
Moayd Ghazzawi <mghazzawi@student.cccd.edu>
2020-02-03 10:43:55 +0000

    auto commit 02/03/20 10:43:54

--- h02/h02.cpp
+++ h02/h02.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  Moayd Ghazzawi
+ *  02/02/2020
  *  @file h02.cpp
  */
 #include <iostream>
@@ -8,18 +8,47 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "mghazzawi";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 /**
- * One line describing what this program does.
+ * This program will determine how much diet soda
+ * can be consumed safely.  The program will ask for
+ * the dieter's target weight to calculate the amount of
+ * diet soda the patient can safely drink.
  * @return 0 for success.
  */
+
+     const double artificialSweetenerPerCan = 35.0;
+     const double poundPerGram = 454.0;
+     double mouseWeightInGrams;
+
+     double lethalDoseMouseInGrams;
+     double dieterWeightInPounds;
+     double dieterWeightInGrams;
+
+     double lethalDoseforFriend;
+     double numberOfCans;
+
+     //Input section
+
 int run()
 {
-    // Add your implementation comments here
 
-    // Write your code here
+    cout <<" Please enter the weight of the mouse in grams: ";
+    cin >> mouseWeightInGrams;
+    cout <<" Please enter the Lethal dose for the mouse (in grams): ";
+    cin >> lethalDoseMouseInGrams;
+    cout << " Please enter the desired weight of the dieter (in pounds): ";
+    cin >>  dieterWeightInPounds;
+
+    //Output section
+
+    dieterWeightInGrams = poundPerGram * dieterWeightInPounds;
+    lethalDoseforFriend = (dieterWeightInGrams / mouseWeightInGrams) * lethalDoseMouseInGrams;
+    numberOfCans = (lethalDoseforFriend / artificialSweetenerPerCan) * 100;
+
+    cout << "Lethal dose in grams, cans is [" << lethalDoseforFriend << fixed << setprecision(0) << ",]";
 
     return 0;
 }
