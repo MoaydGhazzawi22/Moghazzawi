b92ae03e9899d937339f9f4f661d050d7d2640a0
Moayd Ghazzawi <mghazzawi@student.cccd.edu>
2020-02-05 16:59:44 +0000

    auto commit 02/05/20 16:59:44

--- h03/h03.cpp
+++ h03/h03.cpp
@@ -93,7 +93,7 @@ double letterToPoints(stringIn letterGrade){
 }
 
 void printReport(double points){
-	cout << "Grade value is [";
+	cout << "Grade value is" "[";
 	if(points == INVALID_INPUT){
 		cout << "Invalid Input";
 	}else if (points == INVALID_COMBINATION){
