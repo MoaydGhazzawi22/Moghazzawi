00cd53f8c44798e2af5ef35f8942c3c07d9d3dcc
Moayd Ghazzawi <mghazzawi@student.cccd.edu>
2020-02-19 01:06:19 +0000

    auto commit 02/19/20 01:06:18

--- h07/h07.cpp
+++ h07/h07.cpp
@@ -1,13 +1,14 @@
+
 /**
- *  @author Put your name here
- *  @date Put the date here
- *  @file h07.cpp
+ *  @author Moayd Ghazzawi
+ *  @date 02/15/2020
+ *  @file /h07.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "mghazzawi"; // Add your Canvas/occ-email ID
 
 #include "h07.h"
 
@@ -16,8 +17,78 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 
 
 
+
 ////////////////// STUDENT TESTING /////////////
-int run()
+
+
+string zipZap (const string& str)
+{
+    string result="";
+    int len=str.size();
+    if (len<3)
+    {
+        result=str;
+    }
+    else
+    {
+        int i=0;
+        while (i<len-2)
+        {
+            string word=str.substr(i,3);
+            if (word.at(0)=='z'&&word.at(2)=='p')
+            {
+                result +="zp";
+                i+=3;
+
+            }
+            else
+            {
+                result+=word.at(0);
+                i++;
+            }
+        }
+    result+=str.substr(i,len);
+    }
+    return result;
+}
+
+int countCode(const string& str)
+{
+    int result=0;
+    if(str.size()>3)
+    {
+        for(size_t i=0, len=str.size(); i<len-3; ++i)
+        {
+            string word=str.substr(i,4);
+            string front=word.substr(0,2);
+            string back=word.substr(3);
+            if (front=="co" && back=="e")
+            {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+
+string everyNth (const string& str, int n)
+{
+    string result="";
+    int len=str.size();
+    for (int i=0; i<len; i+=n)
+    {
+        result=result+str.at(i);
+    }
+    return result;
+}
+
+bool prefixAgain(const string& str, int n)
+{
+    bool result;
+    return str.substr(1).find(str.substr(0,n))!=string::npos;
+}
+
+int run ()
 {
     cout << "Student testing" << endl;
     return 0;
